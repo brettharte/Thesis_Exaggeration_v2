108a450faaa50319dd637eb32517cda4568e7326
DREXEL\bsh52 <bsh52@drexel.edu>
2017-08-23 19:13:05 -0400

    Warning Screen - Touch Control scene are one scene now.

--- Thesis_Exaggeration/Assets/Scripts/AnyKey_Down.cs
+++ Thesis_Exaggeration/Assets/Scripts/AnyKey_Down.cs
@@ -6,9 +6,17 @@ public class AnyKey_Down : MonoBehaviour
 
     void Update()
     {
-        if (OVRInput.Get(OVRInput.Button.One))
+        if (OVRInput.Get(OVRInput.Button.One)) ;
         {
-            Application.LoadLevel("touch_control");
+            // set the scene up with button and switchable hands:
+            GameObject.Find("button_stand").SetActive(true);
+            GameObject.Find("Touch_controller_L_standard").SetActive(false);
+            GameObject.Find("Touch_controller_R_standard").SetActive(false);
+            GameObject.Find("Voice_Over").SetActive(true);
+            GameObject.Find("LeftHandAnchor").GetComponent<switchHands>().enabled = true;
+            GameObject.Find("RightHandAnchor").GetComponent<switchHands>().enabled = true;
+            // finally destroy ourselves:
+            Destroy(this.gameObject);
         }
     }
 }
