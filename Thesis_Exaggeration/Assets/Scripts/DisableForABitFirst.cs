08d391955e3db18aa3fbcf5de06075b4b9e242b5
strank <strank@strank.info>
2017-08-24 00:17:30 -0400

    Randomized order loading: sets up a shuffled list initally and then uses this one each new button press. Another small script disables the button for a few seconds initially on scene load.

---
+++ Thesis_Exaggeration/Assets/Scripts/DisableForABitFirst.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableForABitFirst : MonoBehaviour {
+    public GameObject target;
+    public float secondsToWait = 2;
+
+    void Awake() {
+        if (target != null) {
+            target.SetActive(false);
+            StartCoroutine(EnableAfterWait(target));
+        }
+    }
+
+    IEnumerator EnableAfterWait(GameObject gameObj) {
+        yield return new WaitForSeconds(secondsToWait);
+        gameObj.SetActive(true);
+    }	
+}
--- Thesis_Exaggeration/Assets/Scripts/Randomized_Order_Loading.cs
+++ Thesis_Exaggeration/Assets/Scripts/Randomized_Order_Loading.cs
@@ -1,16 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// A simple extension to allow shuffling lists
+/// </summary>
+public static class IListExtensions
+{
+    private static System.Random rng = new System.Random();
+
+    /// <summary>
+    /// Shuffles the element order of the specified list using Fisher-Yates shuffle.
+    /// </summary>
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1) {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
+
 
 public class Randomized_Order_Loading : MonoBehaviour {
+    // the possible scenes to be loaded in randomized order:
+    private static IList<int> sceneOrder = new List<int>() { 2, 3, 4 };
+    private static bool not_yet_randomized = true;
+    private static IEnumerator<int> sceneIterator;
 
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
-           // yield WaitForSeconds(10); // wait 10 seconds
-            Application.LoadLevel("softbody_O");
-          
+            if (not_yet_randomized) {
+                not_yet_randomized = false;
+                // determine a random order for the following scenes
+                sceneOrder.Shuffle();
+                // get an iterator that can be used later
+                sceneIterator = sceneOrder.GetEnumerator();
+                // and for now switch to the 0% scene
+                SceneManager.LoadScene("softbody_O");
+            } else {
+                // switch to the next scene in the randomized order
+                if (sceneIterator.MoveNext()) {
+                    SceneManager.LoadScene(sceneIterator.Current);
+                } else {
+                    // end of scenes! TODO
+                }
+            }
         }
     }
 }
