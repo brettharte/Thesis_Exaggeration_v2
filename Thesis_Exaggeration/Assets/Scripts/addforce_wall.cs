9aabf8bcd71ee3b66fd6b1e545bd6e00e5de4247
Brett-PC\Brett <brett.s.harte@gmail.com>
2017-09-25 22:34:41 -0400

    Adding force to walls/cubes

---
+++ Thesis_Exaggeration/Assets/Scripts/addforce_wall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addforce_wall : MonoBehaviour
+{
+    public Rigidbody wall;
+
+    void Update()
+    {
+        if (Input.GetKeyDown ("space")); 
+        wall.GetComponent<Rigidbody>();
+      //-- StartCoroutine("waitTwoSeconds");
+
+    }
+
+    void FixedUpdate()
+    {
+        wall.AddForce(Vector3.back * 75f, ForceMode.Force);
+    }
+
+   //-- IEnumerator waitTwoSeconds()
+    //--{
+        //	yield return new WaitForSeconds (1);
+        //GameObject.FindGameObjectWithTag("cube");
+        //foreach (GameObject target in gameObject); 
+        //{
+        //	gameObject.tag = "cube".AddComponent<Rigidbody> ();	
+        //}	
+        //{
+       //-- yield return new WaitForSeconds(2);
+        //--wall.transform.DetachChildren();
+    }
+//}
+//}
