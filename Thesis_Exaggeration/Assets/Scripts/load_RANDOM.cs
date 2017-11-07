901facb446b5322174215542526d3659e51aff02
Brett-PC\Brett <brett.s.harte@gmail.com>
2017-11-06 22:30:47 -0500

    Wall moving and fall edits

--- Thesis_Exaggeration/Assets/Scripts/load_RANDOM.cs
+++ Thesis_Exaggeration/Assets/Scripts/load_RANDOM.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class load_RANDOM : MonoBehaviour {
 
+    private GameObject Wall_Collider_Fall;
+    private GameObject Falling_wall;
 
-   
+    void Start()
+    {
+        Wall_Collider_Fall = GameObject.Find("Wall_Collider_Fall");
+        Wall_Collider_Fall.gameObject.SetActive(false);
+        Falling_wall = GameObject.Find("Falling_wall");
+        Falling_wall.gameObject.SetActive(false);
+    }
 
-    void OnTriggerEnter(Collider col)
+void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
+        {
+        Wall_Collider_Fall.gameObject.SetActive(true);
+        Falling_wall.gameObject.SetActive(true);
+    }
+    //}
+
+   // void OnTriggerEnter(Collider col)
+   // {
+     //   if (col.gameObject.tag == "Player")
         {
 
             StartCoroutine(newshapes());
@@ -20,7 +37,7 @@ public class load_RANDOM : MonoBehaviour {
     IEnumerator newshapes()
         
         {
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(10);
         Application.LoadLevel(Random.Range(3, 7));
         }
 }
