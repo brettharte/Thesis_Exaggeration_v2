037cde7c5ff0a8b33487eaaa25f2acc7f4039393
DREXEL\bsh52 <bsh52@drexel.edu>
2018-02-14 18:53:49 -0500

    Coding issues solved. Ripple Texutre tested with lighting/shading edits.

---
+++ Thesis_Exaggeration/Assets/ReloadPrefab.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadPrefab : MonoBehaviour {
+    public GameObject toBeReplaced;
+    public GameObject prefabToReload;
+
+	void Update () {
+        if (toBeReplaced != null && prefabToReload != null) {
+            if (Input.GetKeyDown(KeyCode.R)) {
+                Vector3 oldPos = toBeReplaced.transform.position;
+                Quaternion oldRot = toBeReplaced.transform.rotation;
+                Transform oldParent = toBeReplaced.transform.parent;
+                Destroy(toBeReplaced);
+                toBeReplaced = Instantiate(prefabToReload, oldPos, oldRot, oldParent) as GameObject;
+            }
+        }
+    }
+}
